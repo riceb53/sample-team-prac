61fe91682eff14aa56a38614293fa4d2251a35b4
Brian <riceb53@gmail.com>
2018-08-18 22:51:59 -0500

    add new blank line

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     get '/pages' => 'pages#index'
     get '/pages/:id' => 'pages#show'
+
   end
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
